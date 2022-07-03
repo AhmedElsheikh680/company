831ade81b19f96c4f0931bf90ab5d6a3df728b06
Ahmed Elsheikh <ahmedelsheikh680@gmail.com>
2022-07-03 10:27:49 -0700

    create model employee

---
+++ src/main/java/com/company/model/Employee.java
@@ -0,0 +1,30 @@
+package com.company.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="employee")
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name="address")
+    private String address;
+}
